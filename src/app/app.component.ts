f62f891dee3ff52f5a5596ac6408d8043677785f
Yogesh Patil <yogesh.patil@leftrightmind.com>
2023-04-13 14:27:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'flex-page';
+  // model ={option: 'row'};  
+  // selected_direction: string ="";
+
+  options = {
+    direction :  'row',
+    horizontal_direction  : 'start',
+    vertical_direction :  'start',
+  };
+
+  // direction= ['row', 'column'];
+  // horizontal_direction=['none', 'start(default)','center','end','space-around','space-between','space-evenly'];
+  // vertical_direction=['none','start','center','end','stretch(default)'];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MatCardModule } from "@angular/material/card";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatRadioModule } from '@angular/material/radio';
+import {MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MatCardModule, FlexLayoutModule, MatRadioModule,MatFormFieldModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
